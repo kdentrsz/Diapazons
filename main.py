1c2be19d3e229b73ab723676d52d223327d05d9e
Timurs Petrovs <91870903+kdentrsz@users.noreply.github.com>
2021-12-06 08:15:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def noteiktDiapazonu(d1,d2,sk):
+  rezultats = "Skaitlis nav diapazona!"
+  if d1>=sk<=d2:
+    rezultats = "Skaitlis ir diapazona!"
+  return rezultats
+
+d1 = int(input("Ievadi diapazona sakumu: "))
+d2 = int(input("Ievadi diapazona beigas: "))
+sk = int(input("Ievadi skaitli: "))
+rez = noteiktDiapazonu(d1,d2,sk)
+print(rez)
